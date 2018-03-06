b4b080bc9818e5c26ee40c246c725ae0f5b8f1c5
jx <1046278916@qq.com>
2018-03-06 10:47:09 +0800

    's'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel ~
+                    圣财biubiu
                 </div>
 
                 <div class="links">
