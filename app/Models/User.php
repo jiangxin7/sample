e38a70d1773804807b5ada29837d60a56ed57128
jx <1046278916@qq.com>
2018-03-08 09:31:27 +0000

    模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $table='Users';
+    protected $table='users';
     protected $fillable = [
         'name', 'email', 'password',
     ];
